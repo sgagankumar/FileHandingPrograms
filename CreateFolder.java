6424acfc91832d328b9f4ad21778e353053eba18
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2019-04-14 09:07:43 +0530

    Update CreateFolder.java

--- CreateFolder.java
+++ CreateFolder.java
@@ -4,7 +4,7 @@ public class CreateFolder
 {
 	public static void main(String args[])
 	{
-		File f1=new File("C:\\Users\\Gagan\\Desktop\\Image Encryption");
+		File f1=new File("C:\\Users\\Gagan\\Desktop\\Folder");
 		if(f1.exists()==false)
 		{
 			f1.mkdir();
