4eb9df064f4a35880a97561df73b841567784a84
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2019-04-14 09:05:35 +0530

    Create CreateFolder.java

---
+++ CreateFolder.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class CreateFolder 
+{
+	public static void main(String args[])
+	{
+		File f1=new File("C:\\Users\\Gagan\\Desktop\\Image Encryption");
+		if(f1.exists()==false)
+		{
+			f1.mkdir();
+			System.out.println("Folder Created");
+		}
+		else
+		{
+			System.out.println("Folder Already Exists");
+		}
+	}
+}
